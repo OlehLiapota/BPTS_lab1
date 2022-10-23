f9fad88fdfb28fe9cdc7fdc9f5e2f57462030247
Oleh Liapota <oleh.liapota@nixsolutions.com>
2022-10-23 20:31:44 +0300

    Renamed DES to KeyGeneration, added weak keys check

--- lab/src/main/java/Main.java
+++ lab/src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        DES des = new DES();
-        byte[][] subKeys = des.keyToSubKeys("IEOFIT#1");
+        KeyGeneration keyGeneration = new KeyGeneration();
+        byte[][] subKeys = keyGeneration.keyToSubKeys("IEOFIT#");
     }
 }
